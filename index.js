66b2ad14a970072f2dcbecbef1ace9673fcbfa2d
rajesh-puripanda-lab <rajesh.puripanda@icloud.com>
2024-12-15 21:10:42 +0530

    2024-12-15T21:10:42+05:30

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-const date = moment().format();
+// Todays commit
+// const date = moment().format();
+
+// 5 DAYS AGO commit
+const date = moment().subtract(5, "d").format();
+
 console.log(date);
 
 const data = {
   date: date,
 };
 
-jsonfile.writeFile(path, data);
-
-// Todays commit
-simpleGit().add([path]).commit(date, { "--date": date }).push();
+jsonfile.writeFile(path, data, () => {
+  simpleGit().add([path]).commit(date, { "--date": date }).push();
+});
